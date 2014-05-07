8ebdd98dbe54920e9b25b967f7f140e7e602207d
vprajapa <prajapati.vik@gmail.com>
2014-05-07 16:37:49 -0500

    fix when appcmd.exe app runs as set
    fix when appcmd.exe app runs as set make sure the parameter is app.name instead of site.name

--- lib/puppet/provider/iis/iis_object.rb
+++ lib/puppet/provider/iis/iis_object.rb
@@ -113,7 +113,8 @@ class Puppet::Provider::IISObject < Puppet::Provider
   def execute_flush
     if @resource[:ensure] != :absent
       args = get_property_args()
-      appcmd *(['set', self.class.iis_type(), resource[:name]] + args) if args.length > 0
+      #appcmd *(['set', self.class.iis_type(), resource[:name]] + args) if args.length > 0
+      appcmd *(['set', self.class.iis_type()] + get_name_args_for_set() + get_property_args())
     end
   end
 
